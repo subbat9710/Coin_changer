14601ebbb4ec132d9c5760f20c7056f96e29656c
subbat9710 <tula.subba1991@gmail.com>
2017-02-22 13:58:17 -0500

    I am passing 1 cents to penny hash

--- coin_chnger.rb
+++ coin_chnger.rb
@@ -1,3 +1,7 @@
 def make_change(amount)
-	{}
+	if amount == 0
+		{}
+    else
+    	{penny: amount}
+    end
 end
--- test_coin_change.rb
+++ test_coin_change.rb
@@ -3,7 +3,10 @@ require_relative "coin_chnger.rb"
 
 class TestCoinChanger < Minitest::Test
 
-    def test_0_cent_return_empty_hash
+    def test_0_cent_returns_empty_hash
 	    assert_equal({}, make_change(0))
+    end
+    def test_1_cent_returns_penny_hash
+        assert_equal({:penny => 1}, make_change(1)) 
     end
 end
