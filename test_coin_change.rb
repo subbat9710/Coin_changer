98e407280d43096f99be479eba207dd807d969e3
subbat9710 <tula.subba1991@gmail.com>
2017-02-22 15:23:56 -0500

    Test for expecting 2dimes, 1penny.

--- test_coin_change.rb
+++ test_coin_change.rb
@@ -33,4 +33,7 @@ class TestCoinChanger < Minitest::Test
     def test_20_cents_returns_2_dimes_hash
     	assert_equal({:dime => 2}, make_change(20)) #expecting nickel for 5 cents.
     end
+    def test_21_cents_returns_2_dimes_1_penny_hash
+    	assert_equal({:dime => 2, :penny => 1}, make_change(21)) #expecting nickel for 5 cents.
+    end
 end
