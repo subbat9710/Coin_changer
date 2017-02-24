f55e061574c53f404f773d015e10a5da59605a39
subbat9710 <tula.subba1991@gmail.com>
2017-02-24 18:23:01 -0500

    CoinKata/image/layout/css.

--- app.rb
+++ app.rb
@@ -8,6 +8,6 @@ end
 post '/change' do 
   amount = params[:amount].to_i
   change = make_change(amount)
-  value = "change is here #{change}"
+  value = "Your change is here #{change}"
   erb :get_change, :locals => {:value => value}
 end
