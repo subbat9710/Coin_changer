ee593246547d9d2fccd508991ae719f8e25090f0
subbat9710 <teela.subba1991@gmail.com>
2017-07-31 12:37:43 -0400

    Adding the values on the first page.

--- app.rb
+++ app.rb
@@ -8,6 +8,6 @@ end
 post '/change' do 
   amount = params[:amount].to_i
   change = make_change(amount)
-  value = "Your change is here #{change}"
-  erb :get_change, :locals => {:value => value}
+  #value = "Your change is here #{change}"
+  erb :get_change, :locals => {:value => "Your change is here #{change}"}
 end
